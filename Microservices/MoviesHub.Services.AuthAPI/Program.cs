9e249991ef7c3805c200eca33bec64c8efe827de
marco-soria <marco.soriaa7@gmail.com>
2025-04-12 21:00:23 -0500

    added swagger

--- Microservices/MoviesHub.Services.AuthAPI/Program.cs
+++ Microservices/MoviesHub.Services.AuthAPI/Program.cs
@@ -4,9 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
--- Microservices/MoviesHub.Services.MoviesAPI/Program.cs
+++ Microservices/MoviesHub.Services.MoviesAPI/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesHub.Services.MoviesAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MovieDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
